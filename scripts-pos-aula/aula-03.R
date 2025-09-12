4e5ce4a160b10afe6b11fb1db142108e2f5ddd04
Beatriz Milz <42153618+beatrizmilz@users.noreply.github.com>
2025-09-11 21:52:02 -0300

    Add files via upload

---
+++ scripts-pos-aula/aula-03.R
@@ -0,0 +1,538 @@
+# Mapas com ggplot2 -----------------------
+
+## Carregar pacotes
+
+library(sf)
+# Linking to GEOS 3.13.0, GDAL 3.8.5, PROJ 9.5.1; sf_use_s2() is TRUE
+library(geobr)
+library(tidyverse)
+library(abjData)
+
+
+# Pacote sf
+
+# O pacote {sf} (Simple Features for R) (Pebesma 2020, 2018)
+# possibilita trabalhar com bases de dados espaciais.
+
+# Para utilizar esses dados, temos duas abordagens:
+# - Bases que não são georreferenciadas
+#  - Bases que são georreferenciadas
+
+
+## Trabalhando com bases não georreferenciadas -----------------------------------
+
+# Identificar a unidade de análise, e com os joins,
+# unir com uma base georreferenciada.
+
+# O pacote **geobr** (Pereira and Goncalves 2020) é um pacote que
+# disponibiliza funções para realizar o download de diversas bases de
+# dados espaciais oficiais do Brasil. Você pode saber mais no repositório do
+#  pacote no GitHub: https://ipeagit.github.io/geobr/
+
+## Datasets do Brasil ----------------------
+
+# Quais funções para acessar cada dataset: `geobr::list_geobr()`
+
+# geobr::list_geobr()
+
+list_geobr() |> View()
+
+# cuidado com o View()
+
+# mudar o sistema de refencias
+# sf::st_set_crs()
+# Exemplos:
+
+
+# geobr::read_country() # importa a delimitação do Brasil
+
+brasil <- read_country()
+
+glimpse(brasil)
+
+
+brasil |> 
+  ggplot() + 
+  geom_sf()
+
+# geobr::read_state() # importa a delimitação dos estados do Brasil
+
+# lendo dos arquivos (backup)
+estados <- read_rds("dados/geobr/estados.Rds")
+View(estados)
+# abrindo do geobr
+estados <- read_state()
+
+glimpse(estados)
+
+estados
+# Simple feature collection with 27 features and 5 fields
+# Geometry type: MULTIPOLYGON
+# Dimension:     XY
+# Bounding box:  xmin: -66.81025 ymin: -13.6937 xmax: -59.77435 ymax: -7.969294
+# Geodetic CRS:  SIRGAS 2000
+
+estados |>
+  ggplot() +
+  geom_sf(aes(fill = name_region)) 
+
+
+
+estados |> 
+  ggplot() +
+  geom_sf(aes(fill = name_region))
+
+
+mg <- read_state("MG")
+
+mg |> 
+  ggplot() +
+  geom_sf()
+
+# geobr::read_state("DF") # importa a delimitação de um Estado específico,
+#  usando a sigla
+
+# geobr::read_municipality() # importa a delimitação de todos os municípios
+#  do Brasil. É uma base mais pesada!
+
+municipios_todos <- read_rds("dados/geobr/municipios_todos.rds")
+glimpse(municipios_todos)
+
+municipios_todos |>
+  ggplot() + 
+  geom_sf()
+
+
+municipios_sp <- municipios_todos |>
+  filter(abbrev_state == "SP")
+
+municipios_sp |>
+  ggplot() +
+  geom_sf()
+
+# geobr::read_municipality(code_muni = 3550308) # importa a delimitação
+# de um município específico, usando o código do IBGE do município.
+
+
+muni_sp <- read_municipality("SP")
+
+glimpse(muni_sp)
+
+
+osasco <- read_municipality(code_muni = 3534401)
+sp <- read_state("SP")
+
+osasco |> 
+  ggplot() +
+  geom_sf()
+
+
+# Podemos usar esses diferentes objetos como camadas do gráfico ----
+
+ggplot() +
+# geom_sf(data = estados, color = "gray") +
+  geom_sf(data = sp, color = "blue") +
+  geom_sf(data = muni_sp) + 
+  geom_sf(data = osasco, color = "red", fill = "red")
+
+
+
+## Exemplo 1: Trabalhando com os estados brasileiros ------------------
+
+### Importar as bases
+
+# Não usar View()!
+
+# Delimitação dos Estados:
+
+# a) Todos os estados do Brasil
+
+# estados <- readr::read_rds("dados/geobr/estados.Rds")
+
+# Como obter essa mesma base?
+estados <- geobr::read_state()
+
+
+
+# b) Apenas um estado: DF
+
+
+ estado_df <- readr::read_rds("dados/geobr/estado_df.Rds")
+
+# Como obter essa mesma base?
+# estado_df <- geobr::read_state("DF")
+
+# salvar os dados localmente
+write_rds(estado_df, file = "geobr_delimitacao_df.rds")
+# útil para abrir no QGIS
+sf::write_sf(estado_df, "output/geobr_delimitacao_df.shp")
+
+
+
+### Explorar os dados
+
+# Qual é a classe?
+class(estados)
+
+
+
+# Usamos o pacote ggplot2 para criar os mapas, utilizando o geom_sf()
+
+estados |>
+  ggplot() +
+  geom_sf()
+
+
+# Empilhando as bases usando os geom's :
+
+
+starwars |>
+  ggplot() +
+  aes(x = mass, y = height) +
+  geom_point() +
+  geom_smooth()
+
+
+ggplot() +
+  geom_sf(data = estados) +
+  geom_sf(data = estado_df, fill = "pink")
+
+
+ggplot() +
+  geom_sf(data = estados) +
+  geom_sf(data = estado_df, fill = "blue") +
+  theme_bw()
+
+
+
+### Usando o dplyr com objetos sf
+
+# - glimpse() - observar o que a base contém
+
+
+glimpse(estados)
+
+
+
+
+# - filter() - conseguimos filtrar os objetos
+
+
+estados |>
+  filter(name_region == "Nordeste") |>
+  ggplot() +
+  geom_sf()
+
+
+
+# - join()
+
+
+class(abjData::pnud_uf)
+
+abjData::pnud_uf |> View()
+
+class(abjData::pnud_uf)
+
+# não tem geometria!!
+# abjData::pnud_uf |>
+#   ggplot() +
+#   geom_sf()
+
+# quando for fazer join, usar primeiro a base que tem
+# a coluna geom (a base de classe sf)
+
+estados
+
+pnud_uf <- abjData::pnud_uf
+
+# começa com a base sf
+pnud_uf_sf <- estados |>
+  left_join(abjData::pnud_uf, by = c("code_state" = "uf"))
+
+class(pnud_uf_sf)
+
+# base de dados
+pnud_uf_sf |>
+  # inicia o grafico
+  ggplot() +
+  # geometria de mapas, preencher com a coluna espvida
+  geom_sf(aes(fill = espvida)) +
+  # subgráficos por ano
+  facet_wrap(~ano) +
+  # muda escala de cor
+  scale_fill_viridis_c() +
+  # muda tema
+  theme_void()
+
+
+
+
+
+pnud_uf_sf <- estados |>
+  left_join(abjData::pnud_uf, by = c("code_state" = "uf"))
+
+class(pnud_uf_sf)
+
+
+
+### Mapas temáticos ------------------------
+
+
+# glimpse(pnud_uf_sf)
+
+pnud_uf_sf |>
+  filter(ano == 2010) |> 
+  ggplot(aes(fill = idhm)) +
+  geom_sf() +
+  theme_void()
+
+
+
+## Exemplo 2: Escolas em Brasília, DF -----
+
+### Carregar os dados usados
+
+# a) Escolas em Brasília
+
+escolas_brasilia <- readr::read_rds("dados/geobr/escolas_brasilia.Rds")
+
+# Como obter essa mesma base?
+# escolas <- read_schools()
+
+escolas_brasilia <- escolas |> 
+  filter(abbrev_state == "DF", name_muni == "Brasília")
+
+glimpse(escolas_brasilia)
+
+
+# b) Delimitação de Brasília
+
+
+municipio_brasilia <- readr::read_rds("dados/geobr/municipio_brasilia.Rds")
+
+# Como obter essa mesma base?
+# municipio_brasilia <- read_municipality(5300108)
+
+
+
+### Fazer um mapa!
+
+# Passo 1: colocar a delimitação do município e também as escolas
+
+
+ggplot() +
+  geom_sf(data = municipio_brasilia) +
+  geom_sf(data = escolas_brasilia)
+
+
+# Passo 2: Colorir as escolas por "government_level"
+
+
+ggplot() +
+  geom_sf(data = municipio_brasilia) +
+  geom_sf(data = escolas_brasilia, aes(color = government_level))
+
+
+
+# Passo 3: Fazer um facet com "government_level"
+
+
+ggplot() +
+  geom_sf(data = municipio_brasilia) +
+  geom_sf(data = escolas_brasilia, aes(color = government_level)) +
+  facet_wrap(~government_level)
+
+
+
+# Passo 4: Remover a legenda, mudar o tema, adicionar um título e
+# centralizar o título
+
+
+ggplot() +
+  geom_sf(data = municipio_brasilia) +
+  geom_sf(data = escolas_brasilia, aes(color = government_level), show.legend = FALSE, alpha = 0.5) +
+  facet_wrap(~government_level) +
+  theme_void() +
+  labs(title = "Escolas em Brasília (2020) \n") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+
+
+# Exemplo 3 - Rodovias em DF --------------
+
+### Carregar os pacotes
+
+
+library(sf)
+library(geobr)
+library(magrittr)
+
+
+
+# Como buscar a base? Se quiser baixar, remova os comentários (#) dos códigos abaixo:
+
+
+
+# Link que disponibiliza a base
+# u_shp <- "https://www.gov.br/infraestrutura/pt-br/centrais-de-conteudo/rodovias-zip"
+
+# Cirar a pasta para baixar o arquivo
+# dir.create("../dados/shp_rod")
+
+# Fazer o download do arquivo zip
+# httr::GET(u_shp,
+#           httr::write_disk("../dados/shp_rod/rodovias.zip"),
+#           httr::progress())
+
+# Descompactar o zip
+# unzip("../dados/shp_rod/rodovias.zip", exdir = "../dados/shp_rod/")
+
+
+# Importar a base em arquivo .shp
+# rodovias <-
+#   st_read(
+#     "../dados/shp_rod/rodovias.shp",
+#     quiet = TRUE#,
+#     #options = "ENCODING=WINDOWS-1252"
+#   ) |>
+#   # limpar o nome das colunas
+#   janitor::clean_names()
+
+# filtrar para DF
+
+# rodovias_df <- rodovias |>
+#   filter(sg_uf == "DF")
+
+
+
+
+### Importar a base
+
+# Essa base foi gerada a partir do código no chunk anterior.
+
+
+rodovias_df <- readr::read_rds("dados/rodovias_df.Rds")
+
+
+
+### Fazendo um mapa simples
+
+
+ggplot() +
+  geom_sf(data = rodovias_df, aes(color = ds_legenda))
+
+
+
+
+## Juntar os 3 exemplos!
+
+
+# Inicio do ggplot
+ggplot() +
+  # limite do estado
+  geom_sf(data = estado_df, color = "gray") +
+  # escolas
+  geom_sf(data = escolas_brasilia, aes(color = government_level), show.legend = FALSE) +
+  # rodovias
+  geom_sf(data = rodovias_df, color = "black") +
+  # adicionar tema
+  theme_void()
+
+
+
+
+# Praticar com os dados do abjData :)
+# Turma sugerir algum mapa para fazer com os dados do abjData
+# que já estamos usando no curso!
+
+library(tidyverse)
+library(sf)
+library(geobr)
+
+dados_pnud <- read_csv2("dados/base_pnud_min.csv")
+
+dados_pnud_2010 <- dados_pnud |>
+  filter(ano == 2010)
+
+class(dados_pnud_2010)
+
+dados_pnud_2010 |>
+  ggplot() +
+  geom_sf()
+
+dados_pnud_2010_sf <- dados_pnud_2010 |>
+  st_as_sf(coords = c("lon", "lat"), crs = "EPSG:4674")
+
+
+# Consulta CRS
+
+sf::st_crs(estados)
+sf::st_crs(dados_pnud_2010_sf)
+
+# substitui o CRS
+sf::st_set_crs()
+
+# Error in st_transform.sfc(X[[i]], ...) : 
+#   cannot transform sfc object with missing crs
+# CRS - Coordinate Reference System
+
+ggplot() +
+  geom_sf(data = estados) +
+  geom_sf(data = dados_pnud_2010_sf)
+
+
+# pontos - com lat/lon da base
+dados_pnud_2010 |> 
+  # cria a coluna geom baseado nas colunas lat/lon
+  st_as_sf(coords = c("lon", "lat"), crs = "EPSG:4674") |> 
+  ggplot() +
+  geom_sf(aes(color = espvida), alpha = 0.3)
+
+
+# Como abrir arquivos externos, como shapefiles ou geopackages? ----
+
+# ler shapefile, ou geopackage
+# sf::read_sf("arquivo.shp")
+# sf::read_sf("arquivo.gpkg")
+
+
+# escrever um geopackage
+# sf::write_sf(objeto, "nome_arquivo.gpkg")
+# sf::write_sf(objeto, "nome_arquivo.shp")
+
+install.packages("leaflet")
+library(leaflet)
+
+# descobrir o nome dos providers
+leaflet::providers
+
+leaflet_pnud <- dados_pnud_2010 |>
+  filter(uf_sigla == "SP") |>
+  mutate(legenda = paste0(muni_nm, "<br> IDH municipal: ", idhm)) |> 
+  leaflet() |>
+  addProviderTiles("Esri.WorldImagery") |>
+  addMarkers(popup = ~legenda)
+  
+
+htmlwidgets::saveWidget(widget = leaflet_pnud, file = "leaflet_pnud.html")
+
+
+# dúvida dos conflitos
+
+library(dplyr)
+
+# Attaching package: ‘dplyr’
+# 
+# The following objects are masked from ‘package:stats’:
+# 
+#     filter, lag
+# 
+# The following objects are masked from ‘package:base’:
+# 
+#     intersect, setdiff, setequal, union
+
+dplyr::filter()
+
+stats::filter()
