4fae12196ed5db38f2789f93a436e05ef7e20a3a
Beatriz Milz <42153618+beatrizmilz@users.noreply.github.com>
2025-09-13 08:44:05 -0300

    Add files via upload

---
+++ scripts-pos-aula/exemplo-powerpoint.R
@@ -0,0 +1,29 @@
+# exemplo simples: 
+# salvar um gráfico em ppt editável
+
+# carregar pacotes
+library(tidyverse)
+library(esquisse)
+
+# criando um gráfico simples e salvando em um objeto
+grafico_1 <- starwars |>
+  ggplot() +
+  aes(x = height, y = mass) +
+  geom_point()
+
+grafico_2 <- starwars |>
+  mutate(
+    species_fct = forcats::fct_lump_min(species, min = 2)) |> 
+  count(species_fct) |>
+  mutate(species_fct = forcats::fct_reorder(species_fct, n)) |> 
+  ggplot() +
+  aes(x = n, y = species_fct) +
+  geom_col()
+
+# a função ggplot_to_ppt é do pacote esquisse. 
+# ela recebe apenas um argumento: um texto (entre aspas)
+# com o nome do objeto.
+# não indicamos onde o arquivo será salvo, 
+# e a função abre o powerpoint com o resultado.
+ggplot_to_ppt(c("grafico_1", "grafico_2"))
+
